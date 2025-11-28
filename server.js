bfc5233e3fe571eadc8d3bb44a37debe1a5378d1
badal <badalboss2@gmail.com>
2025-11-28 18:59:15 +0530

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const app = express()
 const multer  = require('multer')
 const {mergePdfs}  = require('./merge')
 
-
 const upload = multer({ dest: 'uploads/' })
 app.use('/static', express.static('public'))
 
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"templates/index.html"));
@@ -18,25 +17,23 @@ app.post('/merge', upload.array('pdfs'), async (req, res, next)=> {
 
   console.log(req.files)
 
-   try {
+  try {
     if (!req.files || req.files.length === 0) {
-      
       return res.status(400).send('No PDF files uploaded.Please,check it!');
     }
-  
+
     let filePaths = req.files.map(file => path.join(__dirname, file.path));
-  let d=await mergePdfs(filePaths);
+    let d = await mergePdfs(filePaths);
 
-  res.redirect(`http://localhost:3000/static/${d}.pdf`);
-  // res.send({data:req.files})
+    // relative URL – works locally and on Render
+    res.redirect(`/static/${d}.pdf`);
 
-    }  catch (err) {
+  } catch (err) {
     console.error(err);
     res.status(500).send('Error while merging PDFs.Try again!');
   }
 })
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
